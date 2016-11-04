a8b1e915fd22c8b7d9ddbc7306a4afcf9b84853d
Chris Stryczynski <chris@soundshare.co.uk>
2016-11-04 14:12:05 +0000

    Woooo

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,71 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Lib
 
---https://www.archlinux.de/?page=PackageStatistics
+import Data.Maybe (listToMaybe)
+import Data.Text (Text, unpack, pack)
+import Data.String.Utils (rstrip)
+import Text.XML
+import Text.XML.Cursor
+import Data.Char (isPrint, isSpace)
+import qualified Text.XML.Cursor.Generic
+
+import Control.Monad.Trans.List
+import Control.Monad
+import Control.Monad.Trans.Class
+
+applyLeftRight :: String -> [a] -> Either String [a]
+applyLeftRight errorString x = if (length x) == 0 then Left errorString else Right x
+
+failIfEmpty :: a -> [b] -> Either a [b]
+failIfEmpty reason [] = Left reason
+failIfEmpty _      xs = Right xs
+
+parseDoc :: Document -> Either ([Cursor], String) [Cursor]
+parseDoc doc = do
+    root <- failIfEmpty ([], "no document") $ [fromDocument doc]
+    body <- failIfEmpty (root, "no body!") $ root >>= ($/ element "body")
+    divs <- failIfEmpty (body, "no divs!") $ body >>= ($/ element "div")
+    contents <- failIfEmpty (divs, "no contents!") $ divs >>= attributeIs "id" "content"
+    pure contents
+
 
 main :: IO ()
-main = someFunc
+main = do
+    --doc <- makeRequest "https://www.archlinux.de/?page=PackageStatistics"
+    doc <- getDocumentFile "archlinux.html"
+    --let contents = do
+    let test2 = parseDoc doc
+    case test2 of
+      Left (x, errorString) -> do
+        printCursor x
+        print  $ "failure:" ++ errorString
+      Right x -> do
+        printCursor x
+        print "success"
+
+    -- runListT $ (ListT (Just [1,2,3]) >>=(\x -> ListT $ return [x+1]))
+    -- Just [2,3,4]
+--     let test2 = runListT $
+--                 (   ListT ((applyLeftRight "First " [1,2,3]))
+--                  >>= (\_ -> ListT $ Right [])
+--                  ) :: Either String [Int]
+--     let test2 = runListT $
+--                 (
+--                     (ListT (
+--                         applyLeftRight "First" (fromDocument (doc :: Document) $/ element "body")
+--                     ))
+--                     >>= (\_ -> (ListT (Left "abcxyz")))
+--                 ) :: Either String [Cursor]
+--    let test2 = runListT $
+--                (
+--                    (ListT $ applyLeftRight "First" (fromDocument (doc :: Document) $/ element "body"))
+--                    >>= (\x -> (ListT $ applyLeftRight "Second" (x $/ element "div")))
+--                    >>= (\x -> (ListT $ applyLeftRight "Third" (attributeIs "id" "content")))
+--                ) :: Either String [Cursor]
+
+--             >>= applyLeftRight "Third" (attributeIs "id" "content")
+--             >>= applyLeftRight "Fourth" ($/ element "div")
+--             >>= applyLeftRight "Fifth" (attributeIs "class" "box")
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,66 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Monad.Trans.Resource (runResourceT)
+import Data.Conduit (($$+-))
+import Network.HTTP.Conduit (tlsManagerSettings, http, newManager, parseUrlThrow, responseBody)
+import qualified Text.HTML.DOM as THX (readFile, sinkDoc)
+import Text.XML as TX (Document)
+import Data.Default
+
+import Data.Maybe (listToMaybe)
+import Data.Text (Text, unpack, pack)
+import Data.String.Utils (rstrip)
+import Text.XML
+import Text.XML.Cursor
+import Data.Char (isPrint, isSpace)
+import qualified Text.XML.Cursor.Generic
+
+getDocumentFile :: FilePath -> IO Document
+getDocumentFile path = do
+  THX.readFile path
+
+makeRequest :: String -> IO Document
+makeRequest url = do
+  request <- parseUrlThrow url
+
+  manager <- newManager tlsManagerSettings
+
+  runResourceT $ do
+    -- Actually make the request
+    response <- http request manager
+    -- Extract the response body.
+    let body = responseBody response
+    -- Parse the body as HTML.
+    body $$+- THX.sinkDoc
+
+printCursor cursor = mapM putStrLn ( map (printBasicNode . node) cursor)
+
+printBasicNode :: Node -> String
+printBasicNode nodePrint@(NodeElement element) = mainNode ++ "Chlidren: \n" ++ children where
+  mainNode = (unlines [
+      "Main node: "
+      , printBasicNodeElements nodePrint
+    ])
+  children = concatMap unlines [
+      map (formatChildString . printBasicNodeElements) (elementNodes element)
+    ]
+
+formatChildString :: String -> String
+formatChildString string = rstrip $ unlines $ map (prefixString "    ")  $ lines string
+
+prefixString :: String -> String -> String
+prefixString prefix string = prefix ++ string
+
+printBasicNodeElements :: Node -> String
+printBasicNodeElements (NodeElement element) = unlines
+  [
+    "element: " ++ (unpack $ nameLocalName $ elementName element)
+    , "    attributes: " ++ (show $ elementAttributes element)
+  ]
+
+printBasicNodeElements (NodeContent content) = "element content: " ++ contentString where
+  contentString = show contentStripped
+  contentStripped = filter (not . isSpace) $ unpack content
+printBasicNodeElements (NodeComment comment) = "element comment: " ++ commentString where
+  commentString = show comment
+printBasicNodeElements  a = "element ???:" ++ show a
