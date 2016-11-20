affb754464131aba5e75bcc75672b42f32d86ef1
Chris Stryczynski <chris@soundshare.co.uk>
2016-11-20 15:25:56 +0000

    Minor cleanup.

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,12 +6,11 @@ import Data.Conduit (($$+-))
 import Network.HTTP.Conduit (tlsManagerSettings, http, newManager, parseUrlThrow, responseBody)
 import qualified Text.HTML.DOM as THX (readFile, sinkDoc)
 import Text.XML as TX (Document)
-import Data.Text (Text, unpack, filter)
-import Data.List (filter)
-import Data.String.Utils (rstrip)
+import Data.Text as DT (Text, unpack)
+import qualified Data.Text as DT (strip, words, unwords)
+import Data.String.Utils (strip, rstrip)
 import Text.XML
 import Text.XML.Cursor (node, Cursor)
-import Data.Char (isSpace)
 import qualified Text.XML.Cursor.Generic as XMLG
 
 type CursorParseLeft a = ([Cursor], a)
@@ -37,6 +36,41 @@ makeRequest url = do
     -- Parse the body as HTML.
     body $$+- THX.sinkDoc
 
+formatChildString :: String -> String
+formatChildString string = rstrip . unlines . map prefixStringIndent $ lines string
+
+prefixString :: String -> String -> String
+prefixString prefix string = prefix ++ string
+
+prefixStringIndent :: String -> String
+prefixStringIndent = prefixString "    "
+
+filterString :: String -> String
+filterString = strip . unwords . words
+
+filterText :: Text -> Text
+filterText = DT.strip . DT.unwords . DT.words
+
+cursorContentIs :: Text -> Cursor -> Bool
+cursorContentIs text cursor = (getContent . node $ cursor) == text
+
+contentIs :: Text -> Cursor -> [Cursor]
+contentIs text cursor = case cursorContentIs text cursor of
+  False -> []
+  True -> [cursor]
+
+getContent :: Node -> Text
+getContent (NodeElement e) = mconcat . map getContent $ elementNodes e
+getContent (NodeContent c) = c
+getContent _ = "Ignored: Node Instruction / Node Comment"
+
+failIfEmpty :: error -> [b] -> Either error [b]
+failIfEmpty reason [] = Left reason
+failIfEmpty _      xs = Right xs
+
+extract :: t -> (a -> [b]) -> [a] -> Either ([a], t) [b]
+extract es f x = failIfEmpty (x, es) $ x >>= f
+
 printCursor :: [XMLG.Cursor Node] -> String
 printCursor cursor = concat ( map (printBasicNode . node) cursor)
 
@@ -49,47 +83,15 @@ printBasicNode nodePrint@(NodeElement element) = mainNode ++ "Chlidren: \n" ++ c
   children = concatMap unlines [
       map (formatChildString . printBasicNodeElements) (elementNodes element)
     ]
-printBasicNode (NodeContent content) = prefixString "Content:" $ show . filterString $ unpack content
+printBasicNode (NodeContent content) = prefixString "Content:" . filterString $ unpack content
 printBasicNode _ = "Ignored: Node Instruction / Node Comment"
-
-formatChildString :: String -> String
-formatChildString string = rstrip . unlines . map (prefixString "    ")  $ lines string
-
-prefixString :: String -> String -> String
-prefixString prefix string = prefix ++ string
 
 printBasicNodeElements :: Node -> String
 printBasicNodeElements (NodeElement element) = unlines
   [
     "element: " ++ (unpack . nameLocalName $ elementName element)
-    , prefixString "    " "attributes: " ++ (show $ elementAttributes element)
-  ]
-printBasicNodeElements (NodeContent content) = "element content: " ++ (show . filterString . unpack $ content)
-printBasicNodeElements (NodeComment comment) = "element comment: " ++ show comment
-printBasicNodeElements  a = "element ???:" ++ show a
+    , prefixStringIndent "attributes: " ++ (show $ elementAttributes element)
+ ]
 
-filterString :: String -> String
-filterString = Data.List.filter (not . isSpace)
-
-filterText :: Text -> Text
-filterText = Data.Text.filter (not . isSpace)
-
-contentIs :: Text -> Cursor -> [Cursor]
-contentIs text cursor = case cursorContentIs text cursor of
-  False -> []
-  True -> [cursor]
-
-cursorContentIs :: Text -> Cursor -> Bool
-cursorContentIs text cursor = (getContent $ node cursor) == text
-
-getContent :: Node -> Text
-getContent (NodeElement e) = getContent $ head $ elementNodes e
-getContent (NodeContent c) = c
-getContent _ = ""
-
-failIfEmpty :: error -> [b] -> Either error [b]
-failIfEmpty reason [] = Left reason
-failIfEmpty _      xs = Right xs
-
-extract :: t -> (a -> [b]) -> [a] -> Either ([a], t) [b]
-extract es f x = failIfEmpty (x, es) $ x >>= f
+printBasicNodeElements (NodeContent content) = "element content: " ++ (filterString . unpack $ content)
+printBasicNodeElements _ = "Ignored: Node Instruction / Node Comment"
